3b1f66e9b713fe6dd0a37d9d3799b5c6953ae528
lixianjing <xianjimli@hotmail.com>
2021-04-07 16:01:25 +0800

    sync

--- sync_assets.sh
+++ sync_assets.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-cp -rf ../awtk-quickjs/design .
-
-cp -vf ../awtk-binding/demos/js/* design/default/scripts
-cp -vf ../awtk-binding/tools/code_gen/js/output/awtk.js* design/default/scripts
-cp -vf ../awtk-binding/tools/code_gen/js/output/awtk.js* src/js
+cp -rf ../awtk/design .
 
 python3 scripts/update_res.py clean
 python3 scripts/update_res.py all
