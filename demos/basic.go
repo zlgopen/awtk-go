ec49b68b0becbac63ac3dc9e02a3289e585b00af
lixianjing <xianjimli@hotmail.com>
2021-09-02 11:08:59 +0800

    add demos

---
+++ demos/basic.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/zlgopen/awtk-go/awtk"
+)
+
+func appInit() {
+	win := awtk.TWindowOpen("basic")
+  bar1 := win.Lookup("bar1", true);
+  bar2 := win.Lookup("bar2", true);
+
+  incButton := win.Lookup("inc_value", true);
+	incButton.On(awtk.EVT_CLICK, func(ctx interface{}, e awtk.TEvent) awtk.TRet {
+		fmt.Println("Inc is clicked")
+		bar1.AddValue(10);
+		bar2.AddValue(10);
+		return awtk.RET_OK
+	}, win)
+  decButton := win.Lookup("dec_value", true);
+	decButton.On(awtk.EVT_CLICK, func(ctx interface{}, e awtk.TEvent) awtk.TRet {
+		fmt.Println("Inc is clicked")
+		bar1.AddValue(-10);
+		bar2.AddValue(-10);
+		return awtk.RET_OK
+	}, win)
+}
+
+func main() {
+	awtk.Init(320, 480, awtk.APP_DESKTOP, "demo", "res")
+	awtk.TExtWidgetsInit()
+	awtk.InitAssets()
+	appInit()
+
+	awtk.Run()
+}
